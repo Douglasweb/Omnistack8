7d3d098f4e52da56fc267ae0a93f14e9dce5e3e3
Douglas Eduardo <webdouglasti@gmail.com>
2019-10-31 19:19:25 -0300

    Update routes.js
    correcao rota

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
 const LikeController = require('./controllers/LikeController');
-const DislikeController = require('./controllers/DislikeControlle');
+const DislikeController = require('./controllers/DislikeController');
 
 const routes = express.Router();
 
